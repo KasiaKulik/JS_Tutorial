5737c7a5f9cc70b65e6cd5b3441b88fff879f551
KasiaKulik <54910320+KasiaKulik@users.noreply.github.com>
2019-09-18 18:29:23 +0200

    Add files via upload
    Creating new objects, THIS, constructor functions.

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,27 +1,22 @@
-var myArray = new Array();
-myArray[0] = 8;
-myArray[1] = "hello";
+var Car = function(maxSpeed, driver){
 
-var myCar = new Object();
-myCar.maxSpeed = 50;
-myCar.driver = "Kasia";
-
-console.log(myCar.maxSpeed);
-console.log(myCar.driver);
-
-myCar.drive = function (){console.log("now driving");};
-
-myCar.drive ();
-
-
-var myCar2 = {
-    
-    maxSpeed:70, 
-    driver:"Max", 
-    drive:function (speed, time){
+    this.maxSpeed = maxSpeed;
+    this.driver = "KasiaK";
+    this.drive = function (speed, time){
         console.log(speed * time);
-    } 
-};
+    }
+    this.logDriver = function(){
+        console.log("driver name is "+ this.driver);
+    }
+}
 
-console.log (myCar2.maxSpeed);
-myCar2.drive (50, 3);
+var myCar = new Car(70,"Mike");
+var myCar2 = new Car(50, "Lucky Look");
+var myCar3 = new Car(20,"He-Man");
+var myCar4 = new Car(170,"Spartacus");
+
+myCar.drive(30,5);
+myCar3.logDriver();
+
+
+
