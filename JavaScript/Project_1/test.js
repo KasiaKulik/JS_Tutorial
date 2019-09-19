4b91f6b0b91fff18d1c243d1dbab4d123e655668
KasiaKulik <54910320+KasiaKulik@users.noreply.github.com>
2019-09-19 15:12:31 +0200

    Add files via upload
    Date object, DOM, transfering the DOM.

---
+++ JavaScript/Project_1/test.js
@@ -0,0 +1,27 @@
+var Car = function(maxSpeed, driver){
+
+    this.maxSpeed = maxSpeed;
+    this.driver = "KasiaK";
+    this.drive = function (speed, time){
+        console.log(speed * time);
+    }
+    this.logDriver = function(){
+        console.log("driver name is "+ this.driver);
+    }
+}
+
+var myCar = new Car(70,"Mike");
+var myCar2 = new Car(50, "Lucky Look");
+var myCar3 = new Car(20,"He-Man");
+var myCar4 = new Car(170,"Spartacus");
+
+myCar.drive(30,5);
+myCar3.logDriver();
+
+
+var birthday = new Date (1575,0,14, 02, 22, 33);
+console.log (birthday.getMonth());
+console.log (birthday.getTime());
+
+
+
